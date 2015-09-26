d6b32ce4ea2e0aa590928fdab4f6f85a013841df
poramat <poramat@poramat>
2015-09-27 06:17:06 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,7 +1,20 @@
 <?php
-
+use yii\helpers\Html;
 /* @var $this yii\web\View */
 
 $this->title = 'กรมอนามัย';
 ?>
-Hello Yii
+<?php
+$route1=Yii::$app->urlManager->createUrl('test/test1');
+?>
+<a href="<?=$route1;?>">ไปที่ test1</a>
+<br>
+
+<?php
+$route2=Yii::$app->urlManager->createUrl(['test/test2','name'=>'poramat','lname'=>'pik']);
+?>
+<a href="<?=$route2;?>">ไปที่ test2</a>
+<br>
+
+<?=Html::a('ลิงค์แบบ 3',['test/test1','a'=>'1']);?>
+
